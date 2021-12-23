57494428fc6df699cae9906868bfc827334daa86
victorburle <mail@victorburle.com>
2021-12-23 12:11:20 -0300

    9th commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-import { useState } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
-import {Room} from "@material-ui/icons";
-
-
+import { useEffect, useState } from 'react';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import { Room, Star } from "@material-ui/icons";
+import "./app.css"
+import axios from "axios"
 
 
 function App() {
 
+    const [pins, setPins] = useState([]);
     const [viewport, setViewport] = useState({
         width: "100vw",
         height: "100vh",
@@ -16,6 +17,19 @@ function App() {
 
     });
 
+    useEffect(() => {
+        const getPins = async () => {
+            try {
+                const res = await axios.get("/pins");
+                setPins(res.data);
+            } catch (err) {
+                console.log(err)
+            }
+        };
+        getPins();
+
+    }, []);
+
     return (
 
         <div className="App">
@@ -23,16 +37,47 @@ function App() {
                 {...viewport}
                 mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
                 onViewportChange={nextViewport => setViewport(nextViewport)}
+                mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira"
             >
-                <Marker 
-                latitude={48.858222} 
-                longitude={2.2945} 
-                offsetLeft={-20} 
-                offsetTop={-10}
-                >
-                   <Room/>
+                {pins.map((p) => (  
+                <>  
 
+                <Marker
+                    latitude={p.lat}
+                    longitude={p.long}
+                    offsetLeft={-20}
+                    offsetTop={-10}
+                >
+                    <Room 
+                    style={{ fontSize: viewport.zoom * 7, color: "slateblue" }}
+                 />              
                 </Marker>
+                <Popup
+                    latitude={p.lat}
+                    longitude={p.long}
+                    closeButton={true}
+                    closeOnClick={false}
+                    anchor="left" >
+                   <div className='card'>
+                    <label>Place</label>
+                    <h4 className='place'>{p.title}</h4>
+                    <label>Review</label>
+                    <p className='desc'>{p.desc}</p>
+                    <label>Rating</label>
+                   <div className='stars'>
+                    <Star className="star"/>
+                    <Star className="star"/>
+                    <Star className="star"/>
+                    <Star className="star"/>
+                    <Star className="star"/>
+                   </div>
+                    <label>Information</label>
+                    <span className="username">Created by <b>{p.username}</b></span>
+                    <span className="date">1 hour ago</span>
+                    </div>
+               </Popup>
+               </>
+               ))}
             </ReactMapGL>
         </div>
     );

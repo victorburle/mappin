4e014fac5c048cc6a2a16a4cb4d1d9de6f165301
victorburle <mail@victorburle.com>
2021-12-23 00:11:01 -0300

    8th commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
+function App() {
 
-function App () {
+    const [viewport, setViewport] = useState({
+        width: "100vw",
+        height: "100vh",
+        latitude: -22.985198,
+        longitude:  -43.1897198,
+        zoom: 17
+
+    });
 
     return (
 
         <div className="App">
-       Hello     
+         <ReactMapGL
+         {...viewport}
+         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+         onViewportChange={nextViewport => setViewport(nextViewport)}
+            />
         </div>
     );
 }

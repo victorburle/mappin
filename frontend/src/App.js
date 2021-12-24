2c92b44e316da1eb9f6c23b3260552309c9dd179
victorburle <mail@victorburle.com>
2021-12-23 23:00:36 -0300

    12th commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import { Room, Star } from "@material-ui/icons";
 import "./app.css"
 import axios from "axios"
-import {format} from "timeago.js"
+import { format } from "timeago.js"
+import { Button } from '@material-ui/core';
 
 
 function App() {
@@ -11,6 +12,9 @@ function App() {
     const [pins, setPins] = useState([]);
     const [currentPlaceId, setCurrentPlaceId] = useState(null);
     const [newPlace, setNewPlace] = useState(null);
+    const [title, setTitle] = useState(null);
+    const [desc, setDesc] = useState(null);
+    const [rating, setRating] = useState(0);
     const [viewport, setViewport] = useState({
         width: "100vw",
         height: "100vh",
@@ -34,18 +38,42 @@ function App() {
     }, []);
 
 
-    const handleMarkerClick = (id, lat, long)=>{
+    const handleMarkerClick = (id, lat, long) => {
         setCurrentPlaceId(id)
-        setViewport ({...viewport, latitude:lat, longitude:long});
+        setViewport({ ...viewport, latitude: lat, longitude: long });
     }
 
-    const handleAddClick =  (e) => {
+    const handleAddClick = (e) => {
 
         const [long, lat] = e.lngLat;
-        setNewPlace ({
+        setNewPlace({
             lat,
             long,
         });
+    }
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        const newPin = {
+            username: currentUser,
+            title,
+            desc,
+            rating,    
+            lat: newPlace.lat,
+            long: newPlace.long,
+
+        }
+
+        try {
+            const res = await  axios.post("/pins", newPin);
+            setPins([...pins, res.data]);
+            setNewPlace(null);
+
+        } catch (err) {
+            console.log(err);
+            
+        }
+
     }
 
     return (
@@ -57,84 +85,93 @@ function App() {
                 onViewportChange={nextViewport => setViewport(nextViewport)}
                 mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira"
                 onDblClick={handleAddClick}
-                transitionDuration="200"
+                /*transitionDuration="200"*/
             >
-                {pins.map((p) => (  
-                <>  
+                {pins.map((p) => (
+                    <>
 
-                <Marker
-                    latitude={p.lat}
-                    longitude={p.long}
-                    offsetLeft={-20}
-                    offsetTop={-10}
-                >
-                    <Room 
-                    style={{ 
-                        fontSize: viewport.zoom * 7, 
-                        color: p.username===currentUser ? "tomato" : "slateblue", 
-                        cursor: "pointer" }}
-                    onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
-                 />              
-                </Marker>
-                {p._id === currentPlaceId && (
-                <Popup
-                    latitude={p.lat}
-                    longitude={p.long}
-                    closeButton={true}
-                    closeOnClick={false}
-                    anchor="left" 
-                    onClose={()=>setCurrentPlaceId(null)}  
-                    >
-                    
-                   <div className='card'>
-                    <label>Place</label>
-                    <h4 className='place'>{p.title}</h4>
-                    <label>Review</label>
-                    <p className='desc'>{p.desc}</p>
-                    <label>Rating</label>
-                   <div className='stars'>
-                    <Star className="star"/>
-                    <Star className="star"/>
-                    <Star className="star"/>
-                    <Star className="star"/>
-                    <Star className="star"/>
-                   </div>
-                    <label>Information</label>
-                    <span className="username">Created by <b>{p.username}</b></span>
-                    <span className="date">{format(p.createdAt)}</span>
-                    </div>
-               </Popup>
-                 ) }
-               </>
-               ))}
+                        <Marker
+                            latitude={p.lat}
+                            longitude={p.long}
+                            offsetLeft={-viewport.zoom * 3.5}
+                            offsetTop={-viewport.zoom * 7}
+                        >
+                            <Room
+                                style={{
+                                    fontSize: viewport.zoom * 7,
+                                    color: p.username === currentUser ? "tomato" : "slateblue",
+                                    cursor: "pointer"
+                                }}
+                                onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
+                            />
+                        </Marker>
+                        {p._id === currentPlaceId && (
+                            <Popup
+                                latitude={p.lat}
+                                longitude={p.long}
+                                closeButton={true}
+                                closeOnClick={false}
+                                anchor="left"
+                                onClose={() => setCurrentPlaceId(null)}
+                            >
+
+                                <div className='card'>
+                                    <label>Place</label>
+                                    <h4 className='place'>{p.title}</h4>
+                                    <label>Review</label>
+                                    <p className='desc'>{p.desc}</p>
+                                    <label>Rating</label>
+                                    <div className='stars'>
+                                       {Array(p.rating).fill(<Star classeName="star"/>)}
+                                    </div>
+                                    <label>Information</label>
+                                    <span className="username">Created by <b>{p.username}</b></span>
+                                    <span className="date">{format(p.createdAt)}</span>
+                                </div>
+                            </Popup>
+                        )}
+                    </>
+                ))}
                 {newPlace && (
-                 <Popup 
-                    latitude={newPlace.lat} 
-                    longitude={newPlace.long}
-                    closeButton={true}
-                    closeOnClick={false}
-                    anchor="left" 
-                    onClose={()=>setNewPlace(null)}       
-                >
-                    <div>
-                        <form>
-                            <label>Title</label>
-                            <input placeholder="enter a title"></input>
-                            <label>Review</label>
-                            <textarea placeholder="Say something about this place"></textarea>
-                            <label>Rating</label>
-                            <select>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            </select>
-                            <button className="submitButton" type="submit">Add Pin</button>
-                        </form>
-                    </div>
+                    <Popup
+                        latitude={newPlace.lat}
+                        longitude={newPlace.long}
+                        closeButton={true}
+                        closeOnClick={false}
+                        anchor="left"
+                        onClose={() => setNewPlace(null)}
+                    >
+                        <div>
+                            <form onSubmit={handleSubmit}>
+                                <label>Title</label>
+                                <input
+                                    placeholder="enter a title"
+                                    onChange={(e) => setTitle(e.target.value)}
+                                />
+                                <label>Review</label>
+                                <textarea
+                                    placeholder="Say something about this place"
+                                    onChange={(e) => setDesc(e.target.value)} 
+                                />
+                                <label>Rating</label>
+                                <select onChange={(e)=> setRating(e.target.value)}>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select>
+                                <button className="submitButton" type="submit">Add Pin</button>
+                            </form>
+                        </div>
 
-                 </Popup>)}
+                    </Popup>)}
+                    {currentUser ? (<button className="button logout">Logout</button>
+                    ):(
+                         <div className="buttons">
+                            <button className="button login">Login</button>
+                            <button className="button register">Register</button>
+                        </div>)}
             </ReactMapGL>
         </div>
     );
